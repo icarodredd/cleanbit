79fa18e0d1e44c9b52a35c67d13b3c94967a13ee
icarodredd <icarog854@gmail.com>
2025-05-29 09:53:34 -0300

    feat: macbook scroll component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { Card } from "@/components/card";
-import HeroSection from "@/components/hero-section";
+import { Card } from "@/components/Card";
+import HeroSection from "@/components/HeroSection";
+import { MacScroll } from "@/components/MacScroll";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Card title="Card 1" description="This is the first card" />
         <Card title="Card 1" description="This is the first card" />
       </div>
+      <MacScroll title="This is a Mac Scroll" />
     </>
   );
 }
