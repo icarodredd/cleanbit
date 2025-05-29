78576a94590803398d0a46dedf82690af267715a
icarodredd <icarog854@gmail.com>
2025-05-29 09:20:53 -0300

    feat: add card component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://picsum.photos/1000")],
+  },
 };
 
 export default nextConfig;
